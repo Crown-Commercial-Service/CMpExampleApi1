fa6d9ec3f2e5f1c48d98487ae6960edae92dc443
unknown <42997213+mkrowe@users.noreply.github.com>
2018-10-04 11:20:22 +0100

    Added placeholder pre/post pipline build scripts

---
+++ pipeline_post_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# This is the post-build script
+# Anything in here will be called just after the containers are built, but before they are deployed
+#
+echo 'Running post-build script'
---
+++ pipeline_pre_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# This is the pre-build script
+# Anything in here will be called just before the containers are built
+#
+echo 'Running pre-build script'
